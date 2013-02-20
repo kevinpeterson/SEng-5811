cf0e596dd6e9f734e45a1bb03c39e3414c60c56c
Kevin Peterson <kevin.peterson@mayo.edu>
2013-02-19 22:56:20 -0600

    Updated printouts.

--- homework3/part1.py
+++ homework3/part1.py
@@ -1,14 +1,27 @@
-#                
-#                0
-#               /|
-#              1 |
-#               \|
-#                2 
-#               / \          
-#              3   4
-#               \ / \\
-#                6    5
-#
+print '''
+##############
+Kevin Peterson
+SEng 5811 
+Homework #3
+Part 1
+##############
+'''
+
+graph = '''                 
+                0
+               /|
+              1 |
+               \|
+                2 
+               / \          
+              3   4
+               \ / \\\\
+                6    5
+'''
+
+print "Graph:"
+print graph
+
 graph = {'0': ['1', '2'],
            '1': ['2'],
            '2': ['3', '4'],
@@ -44,11 +57,49 @@ def find_paths(start,graph,visits,path=[]):
                 
         return paths
     
-
-print "\n"           
+print "(a) Paths:"           
 for path in find_paths('0',graph,{'4':1}): 
     print "->".join(str(p) for p in path)
     
-print "\n"            
+print "\n(b) Paths:"          
 for path in find_paths('0',graph,{'4':2,'5':1}): 
     print " -> ".join(str(p) for p in path)
+    
+    
+def _get_cover_sets(main_set,cover_sets):
+    '''Cover Set Algorithm'''
+    cover_set = []
+    
+    while len(main_set) > 0:
+        most_matches = 0
+        best_subset = None
+            
+        for candidate_set in cover_sets:
+            matches = len(main_set.intersection(candidate_set))
+            if matches > most_matches:
+                most_matches = matches
+                best_subset = candidate_set;
+    
+        main_set = main_set.difference(best_subset)
+        cover_sets.remove(best_subset)
+        cover_set.append(best_subset)
+    
+    return cover_set;
+    
+ 
+graph_nodes = set()
+
+paths = find_paths('0',graph,{'4':1});
+for path in paths: 
+    for node in path:
+        graph_nodes.add(node)
+                
+cover_paths = _get_cover_sets(graph_nodes,paths)
+      
+print "\n(c) Test Coverage:"
+print "There are %i path(s) that will cover all nodes in the graph: " % len(cover_paths)
+for cover_path in cover_paths:
+    print " -> ".join(str(p) for p in cover_path)
+
+ 
+    
--- homework3/part2.py
+++ homework3/part2.py
@@ -1,3 +1,12 @@
+print '''
+##############
+Kevin Peterson
+SEng 5811 
+Homework #3
+Part 2
+##############
+
+'''  
 '''
 Predicate:     ((a + b) < c and not p) or (r > s)
 Constraints:    t1: > F = 
@@ -6,31 +15,33 @@ Constraints:    t1: > F =
                 t4: < F = 
                 t5: > F >
 '''
+constraints = dict(t1='> F =',t2='= F <',t3='< T =',t4='< F =',t5='> F >')
+
 
 #Correctly implemented predicate
 def correct_predicate(a,b,c,p,r,s): 
-    '((a + b) < c and not p) or (r > s)'
+    '(( a + b ) < c and not p ) or ( r > s )'
     return ((a + b) < c and not p) or (r > s)
 
 def bad_predicate1(a,b,c,p,r,s): 
-    '((a + b) < c and not p) or (r > s)'
-    return ((a + b) < c or not p) or (r < s)
+    '(( a + b ) < c and p ) or ( r > s )'
+    return ((a + b) < c and p) or (r > s)
 
 def bad_predicate2(a,b,c,p,r,s): 
-    '((a + b) < c and not p) or (r < s)'
-    return ((a + b) < c and not p) or (r < s)
+    '(( a + b ) < c or not p ) or ( r > s )'
+    return ((a + b) < c or not p) or (r > s)
 
 def bad_predicate3(a,b,c,p,r,s): 
-    '((a + b) < c and not p) and (r > s)'
+    '(( a + b ) < c and not p ) and ( r > s )'
     return ((a + b) < c and not p) and (r > s)
 
 def bad_predicate4(a,b,c,p,r,s): 
-    '((a + b) < c and p) and (r > s)'
-    return ((a + b) < c and p) and (r > s)
+    '(( a + b ) > c and not p ) or ( r < s ) '
+    return ((a + b) > c and not p) or (r < s)
 
 def bad_predicate5(a,b,c,p,r,s): 
-    '((a - b) > c and p) and (r > s)'
-    return ((a - b) > c and p) and (r > s)
+    '(( a + b ) > c and p ) and ( r > s )'
+    return ((a + b) > c and p) and (r > s)
 
 test_predicates = [bad_predicate1, bad_predicate2,
                     bad_predicate3, bad_predicate4,
@@ -38,31 +49,45 @@ test_predicates = [bad_predicate1, bad_predicate2,
 
 # Test Cases for -> ((a + b) < c and not p) or (r > s)
 # With Given Constraints
-t1 = dict(a=1,b=2,c=1,p=False,r=5,s=5)# t1: > F =
-t2 = dict(a=1,b=1,c=2,p=False,r=5,s=4)# t2: = F < 
+t1 = dict(a=1,b=1,c=1,p=True,r=5,s=5)# t1: > F =
+t2 = dict(a=1,b=1,c=2,p=True,r=5,s=4)# t2: = F < 
 t3 = dict(a=1,b=1,c=3,p=False,r=5,s=5)# t3: < T = 
 t4 = dict(a=1,b=1,c=3,p=True, r=5,s=5)# t4: < F = 
-t5 = dict(a=1,b=1,c=1,p=False,r=5,s=4)# t5: > F >
+t5 = dict(a=1,b=1,c=1,p=True,r=5,s=4)# t5: > F >
 
 tests = [t1,t2,t3,t4,t5]
 
-def test(test_predicate):
+def run_test_predicates(test_predicate):
     '''Test a test predicate against the known correct one,
     using test cases for all of the defined constraints'''
     
+    print '==========================='
+    print 'Testing Incorrect Predicate'    
+    print '==========================='
     print "Correct predicate: %s" % correct_predicate.__doc__
     print "Test predicate:    %s" % test_predicate.__doc__
-
+    print "  ================="
+    print "  Test Differences:"
+    print "  ================="
     for i,test in enumerate(tests, start=1):
-        if (not (correct_predicate(**test) == test_predicate(**test))):
-            print "  -- Test %i yielded different results" % i  
+        correct_predicate_eval = correct_predicate(**test) 
+        test_predicate_eval = test_predicate(**test)
+        if (not (correct_predicate_eval == test_predicate_eval)):
+            print "  -- Test %i (Constraint: '%s') yielded different results:" % (i, constraints['t'+str(i)])
+            print "      -- Test values were: " + str(test)
+            print "      -- Good Predicate: " + _add_variables_to_predicate(correct_predicate,test) + " == " + str(correct_predicate_eval)
+            print "      -- Test Predicate: " + _add_variables_to_predicate(test_predicate,test) + " == " + str(test_predicate_eval)
             
     print 
 
+def _add_variables_to_predicate(predicate,values):
+    return reduce(lambda x, y: x.replace(' ' +y+' ', ' '+str(values[y])+' ', 1), values, predicate.__doc__)
+
 # Part II (a)
 # Test all of the test predicates
+print('(a)')
 for test_predicate in test_predicates:           
-    test(test_predicate)
+    run_test_predicates(test_predicate)
 
 
 #####################################################################
@@ -71,9 +96,10 @@ for test_predicate in test_predicates:
 # When there is a case where the test predicate makes only one
 # test-case fail, add that test case to set (F). Take `T Difference F` -
 # and the result is unnecessary tests.
+print('(b)')
 
 def test_eval(first,second,third,fourth,fifth,a,b,c,p,r,s):
-    return eval("((a %s b) < c %s %s p) %s (r %s s)" % (first, second, third, fourth, fifth))
+    return eval("((a + b) %s c %s %s p) %s (r %s s)" % (first, second, third, fourth, fifth))
 
 and_or = ['and', 'or']
 not_nothing = ['not', '']
@@ -100,5 +126,14 @@ unnecessary_constraints = all_constraints - necessary_constraints
 if(len(unnecessary_constraints) == 0):
     print "No unnecessary constraints."
 else:
-    print "Unnecessary constraints: " + ",".join(str(s) for s in unnecessary_constraints)
-        
+    print "Unnecessary constraints: " + ",".join("'"+constraints['t'+str(s)]+"'" for s in unnecessary_constraints)
+    print "Approach:"
+    print "* Brute force... I took all permutations of ((a + b) < c and not p) or (r > s)"
+    print "* and tested all possible permutations against the set of all test cases T."
+    print "* If there was a permutation where the defect was caught by only ONE test, I"
+    print "* assumed that test was necessary and added that test to set N. Finally, I took"
+    print "* T difference N, and the result was the set of unnecessary tests."
+
+
+
+    
